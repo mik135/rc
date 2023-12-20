5f9bcf5b55fdbecf2ae8fff4b37713028e89eb05
mik135 <michaelsavior1@gmail.com>
2023-12-20 17:00:38 +0100

    Add bootstrap link to html

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
